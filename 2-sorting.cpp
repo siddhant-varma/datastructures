410d8aa0ac03f36a61c0310be549a87a932c08f5
siddhant-varma <siddhantvarma@outlook.com>
2017-10-15 14:15:09 +0530

    bubble sort added

--- 2-sorting.cpp
+++ 2-sorting.cpp
@@ -19,13 +19,31 @@ class sort{
 				cin>>list[i];
 			}
 		}
-		
 		void show(void){
 			cout<<"\nList is:";
 			for(int i = 0; i < size; i++){
 				cout<<"\t"<<list[i];
 			}
 		}
+		
+		void bubbleSort(void){
+			t hold;
+			int i, j;
+			bool switched = true;
+			
+			for(i = 0; i < size - 1 && switched; i++){
+				switched = false;
+				for(j = 0; j < size - i - 1; j++){
+					if(list[j] > list[j+1]){
+						hold = list[j+1];
+						list[j+1] = list[j];
+						list[j] = hold;
+						switched = true;
+					}
+				}
+			}
+		}
+
 };
 
 int main(void){
@@ -34,6 +52,17 @@ int main(void){
 	cin>>temp;
 	sort<int> array(temp);
 	array.initialize();
+	array.show();
+	cout<<"\nEnter:\t1. Bubble Sort:\t";
+	cin>>temp;
+	switch(temp){
+		case 1:
+			array.bubbleSort();
+			break;
+		default:
+			cout<<"Wrong Choice entered...";
+			break;
+	}
 	array.show();
 	
 	return 0;
