c84a56635f62df0760c4fc91cf484be1afda63ab
siddhant-varma <siddhantvarma@outlook.com>
2017-10-15 14:02:30 +0530

    basic template class sort made

--- 2-sorting.cpp
+++ 2-sorting.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+template<class t>
+class sort{
+	private:
+		t *list;
+		int size;
+	public:
+		sort(int a){
+			size = a;
+			list = new t[size];
+		}
+		void initialize(void){
+			cout<<"";
+			for(int i = 0; i < size; i++){
+				cout<<"\tEnter element at "<<i+1;
+				cin>>list[i];
+			}
+		}
+};
+
 int main(void){
+	int temp;
+	cout<<"Enter size of list: ";
+	cin>>temp;
+	sort<int> array(temp);
+	
 	return 0;
 }
