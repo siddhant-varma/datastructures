3ba0a536b12f9e8d9b94ddd248ef20beda728fa7
siddhant-varma <siddhantvarma@outlook.com>
2017-10-15 19:59:07 +0530

    Simplifed and fully working

--- queue.cpp
+++ queue.cpp
@@ -8,47 +8,38 @@ struct queue{
 	int front, rear, size;
 	int *arr;
 	bool isFull(void){
-		return front==0 && rear==size-1 || front == rear + 1;
+		return front==0 && rear==size-1;
 	}
 	bool isEmpty(void){
-		return front== -1;
+		return front == -1 || front == rear + 1;	//	front < 0 || front > rear
 	}
 	void enqueue(int);
 	int dequeue(void);
 };
 
 void create(queue &q, int siz){
-	q.front=q.rear= -1;
+	q.front= 0;
+	q.rear= -1;
 	q.size = siz;
 	q.arr = new int[siz];
 }
 
 void queue::enqueue(int element){
 	if(!isFull()){
-		if(rear == size -1 || rear == -1){	//for very first entry in queue
-			arr[0] = element;
-			rear=0;	//	intialinzing rear to 0
-			if (front == -1){	//initialinzg front to 0
-				front = 0;
-			}
-		}
-		else
-			arr[++rear] = element;	//for rest of the entries	first increment rear then add
+		arr[++rear] = element;
 	}
 	else
 		cout<<"\nQueue Full.";
 }
 
 int queue::dequeue(void){
-	int tem=arr[front];		//sampling first entry of queue from front
-	if(front == rear){	//empty queue?
-		rear= front= -1;
+	int tem;
+	if(!isEmpty())
+		tem=arr[front++];
+	else{
+		cout<<"\nQueue Empty...";
+		return -1;
 	}
-	else if (front == size -1){
-		front = 0;
-	}
-	else
-		front++;
 	return tem;
 }
 
@@ -58,17 +49,17 @@ int main(void){
 	queue q;
 	cout<<"Enter size of queue: ";
 	cin>>s;
-	create(q,s+1);
-	for(int i=0;i<s;i++){
+	create(q,s);
+	/*for(int i=0;i<s;i++){
 		cout<<"Enter "<<i+1<<" element in queue: ";
 		cin>>t;
 		q.enqueue(t);
-	}
+	}*/
 	
-	cout<<"\nEntered Queue is: ";
+	/*cout<<"\nEntered Queue is: ";
 	for(int i=0;i<s;i++){
 		cout<<q.arr[i]<<"\t";
-	}
+	}*/
 	
 	do{
 		cout<<"\nPress:\n1)\tFor Insertion of Element\n2)\tFor Deletion of Element\n3)\tFor printing queue:";
