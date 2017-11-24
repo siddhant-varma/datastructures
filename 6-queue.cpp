5e92e395c03d54be651070b99dd59fa3b9e69ff8
siddhant-varma <siddhantvarma@outlook.com>
2017-11-24 15:03:44 +0530

    Initial template

---
+++ 6-queue.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Queue{
+	int size, front, rear;
+	T *arr;
+	public:
+		Queue(int s){
+			size = s;
+			front = rear = 0;
+		}
+		
+		bool isEmpty(void){
+			return ();
+		}
+		
+		bool isFull(void){
+			return ();
+		}
+		
+		void enqueue(T el){
+			
+		}
+		
+		T dequeue(void){
+			
+		}
+};
+
+int main(void){
+	
+	return 0;
+}
