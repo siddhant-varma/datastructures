f3efea9b564999b6a60605ba1894b1460281763a
siddhant-varma <siddhantvarma@outlook.com>
2017-11-24 20:26:38 +0530

    Circular queue working

--- 6-queue.cpp
+++ 6-queue.cpp
@@ -9,27 +9,70 @@ class Queue{
 	public:
 		Queue(int s){
 			size = s;
-			front = rear = 0;
+			front = rear = -1;
+			arr = new T[size];
 		}
 		
 		bool isEmpty(void){
-			return ();
+			return (front == rear && rear == -1);
 		}
 		
 		bool isFull(void){
-			return ();
+			return ((rear + 1)%size == front);
 		}
 		
 		void enqueue(T el){
-			
+			if(isFull()){
+				cout<<"\nQueue is Full...\n";
+			}
+			else if(isEmpty()){
+				front = rear = 0;
+				arr[front] = el;
+			}
+			else{
+				rear = (rear + 1) % size;
+				arr[rear] = el;
+			}
 		}
 		
 		T dequeue(void){
-			
+			int temp = front;
+			if(isEmpty()){
+				cout<<"\nQueue is empty...";
+				return -1;
+			}
+			else if(front == rear){
+				front = rear = -1;
+			}
+			else{
+				front = (front + 1) % size;
+			}
+			return arr[temp];
 		}
 };
 
 int main(void){
-	
-	return 0;
+	int temp;
+	cout<<"Enter size of Queue:\t";
+	cin>>temp;
+	Queue <int>q(temp);
+	while(true){
+		cout<<"\nEnter\t1.Enqueue\n\t2.Dequeue\n\t3.Exit\t:";
+		cin>>temp;
+		switch(temp){
+			case 1:{
+				int t;
+				cout<<"Enter element:\t";
+				cin>>t;
+				q.enqueue(t);
+				break;
+			}
+			case 2:{
+				cout<<endl<<q.dequeue()<<" dequeued.";
+				break;
+			}
+			case 3:
+				return 0;
+		}
+	}
 }
