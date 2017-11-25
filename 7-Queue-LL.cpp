4f3bf752f336aba207212ca65957c662e6e31d9c
siddhant-varma <siddhantvarma@outlook.com>
2017-11-25 16:11:02 +0530

    Fully working

--- 7-Queue-LL.cpp
+++ 7-Queue-LL.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 template <class T>
 class Node{
-	T data;
 	public:
+	T data;
 		Node<T> *next;
 		Node(){
 			next = 0;
@@ -53,6 +53,54 @@ class List{
 			}
 		}
 		void display(void){
-			
+			Node <T>* temp;
+			cout<<endl;
+			for(temp = head; temp != 0; temp = temp -> next)
+				cout<<temp -> data<<" << ";
+			cout<<endl;
 		}
 };
+
+template <class T>
+class Queue{
+	public:
+		List<int> lst;
+		void enqueue(T el){
+			lst.addToTail(el);
+		}
+		T dequeue(void){
+			T el = lst.delHead();
+			if(el != -1){
+				cout<<endl<<el<<" dequeued\n";
+			}
+			else{
+				cout<<"\nQueue is Empty...\n";
+			}
+			return el;
+		}
+};
+
+int main(void){
+	Queue<int> q;
+	int temp, el;
+	while(true){
+		cout<<"\nEnter\t1.Enqueue\n\t2.Dequeue\n\t3.Display\n\t4.Exit:\t";
+		cin>>temp;
+		switch(temp){
+			case 1:{
+				cout<<"\nEnter Element:\t";
+				cin>>el;
+				q.enqueue(el);
+				break;
+			}
+			case 2:{
+				q.dequeue();
+				break;
+			}
+			case 3:	q.lst.display();
+					break;
+			case 4:	return 0;
+		}
+	}
+	return 0;
+}
