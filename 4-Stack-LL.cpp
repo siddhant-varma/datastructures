a4741a2b2946b33c1e52e2abf98c6589c03b7a5a
siddhant-varma <siddhantvarma@outlook.com>
2017-11-25 14:23:45 +0530

    Fully Working

--- 4-Stack-LL.cpp
+++ 4-Stack-LL.cpp
@@ -17,7 +17,7 @@ class Node{
 		}
 };
 
-tmeplate<class T>
+template<class T>
 class List{
 	public:
 		Node <T> *head, *tail;
@@ -40,18 +40,83 @@ class List{
 		
 		T delTail(void){
 			if(!isEmpty()){
-				Node<T> *temp;
-				for(temp = head; temp -> next != tail; temp = temp -> next);
 				T el = tail -> data;
-				tail = temp;
-				delete temp;
+				Node<T> *temp;
+				if(head == tail){
+					head = tail = 0;
+					delete temp;
+				}
+				else{
+					for(temp = head; temp -> next != tail; temp = temp -> next);
+					delete tail;
+					tail = temp;
+					tail -> next = 0;
+				}
 				return el;
 			}
 			return -1;
 		}
+		
+		void display(void){
+			Node <T> *temp;
+			cout<<endl;
+			for(temp = head; temp != 0; temp = temp -> next){
+				cout<<temp -> data <<" << ";
+			}
+			cout<<endl;
+			return;
+		}
+};
+
+template <class T>
+class Stack{
+	public:
+		
+		List<int> lst;
+		bool isEmpty(void){
+			return lst.isEmpty();
+		}
+		void push(T el){
+			lst.addtoTail(el);
+		}
+		T pop(void){
+			T el = lst.delTail();
+			if(el != -1){
+				cout<<endl<<el<<" poped from stack.";
+				return el;
+			}
+			else{
+				cout<<"\nStack underFlow...\n";
+				return el;
+			}
+		}
 };
 
 int main(void){
-	
+	Stack <int> stk;
+	int temp, el;
+	while(true){
+		cout<<"\nEnter:\t1.Push\n\t2.Pop\n\t3.Clear\n\t4.Display\n\t5.Exit:\t";
+		cin>>temp;
+		switch(temp){
+			case 1:{
+				cout<<"\nEnter Element:\t";
+				cin>>el;
+				stk.push(el);
+				break;
+			}
+			case 2:stk.pop();
+					break;
+			case 4:{
+				if(!stk.isEmpty())
+					stk.lst.display();
+				else
+					cout<<"\nStack underFlow...\n";
+				break;
+			}
+			case 5:
+				return 0;
+		}
+	}
 	return 0;
 }
