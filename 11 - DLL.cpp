ac56e126cca387c59249b2f33afdeb3dda0acfd7
siddhant-varma <siddhantvarma@outlook.com>
2017-11-25 19:46:11 +0530

    DLL basic func defined

--- 11 - DLL.cpp
+++ 11 - DLL.cpp
@@ -23,22 +23,80 @@ class DList{
 		Dlist(){
 			head = tail = nullptr;
 		}
+		bool isEmpty(void){
+			return head == nullptr;
+		}
 		void addToHead(T el){
-			
+			Node <T> *newNode = new Node<T>(el);
+			if(head == 0){
+				head = tail = newNode;
+			}
+			else{
+				head -> prev = newNode;
+				newNode -> next = head;
+				head = newNode;
+			}
+			return;
 		}
 		void addtoTail(T el){
-			
+			Node<T> *newNode = new Node<T>(el);
+			if(head == 0){
+				head = tail = newNode;
+			}
+			else{
+				tail -> next = newNode;
+				newNode -> prev = tail;
+				tail = newNode;
+			}
+			return;
 		}
 		T delHead(void){
-			
+			if(!isEmpty()){	
+				T el = head -> data;
+				Node<t> *oldNode = head;
+				if(head == tail){
+					head = tail = nullptr;
+				}
+				else{
+					head = head -> next;
+					head -> prev = nullptr;
+				}
+				delete oldNode;
+				return el;
+			}
+			else{
+				cout<<"\nList is empty...\n";
+				return -1;
+			}
 		}
 		T delTail(void){
-			
+			if(!isEmpty()){
+				T el = tail -> data;
+				Node <T> *oldNode = tail;
+				if(head == tail){
+					head = tail = nullptr;
+				}
+				else{
+					tail = tail -> prev;
+					tail -> next = nullptr;
+				}
+				delete oldNode;
+				return el;
+			}
+			else{
+				cout<<"\nList is empty...";
+				return -1;
+			}
 		}
 		void display(void){
+			Node <T> *temp;
+			cout<<endl;
+			for(temp = head; temp != nullptr; temp = temp -> next)
+				cout<<temp -> data << " << ";
 			
+			cout<<endl;
+			return;
 		}
-		
 };
 
 int main(void){
