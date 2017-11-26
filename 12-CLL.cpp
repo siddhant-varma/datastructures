bab6bf603fd221002327605fe83c176f8893f431
siddhant-varma <siddhantvarma@outlook.com>
2017-11-26 13:09:48 +0530

    addToHead() & display() - FINE

--- 12-CLL.cpp
+++ 12-CLL.cpp
@@ -10,7 +10,7 @@ class Node{
 		Node(){
 			next = nullptr;
 		}
-		Node(T el, Node ptr = nullptr){
+		Node(T el, Node *ptr = nullptr){
 			data = el;
 			next = ptr;
 		}
@@ -32,8 +32,41 @@ class CList{
 				tail -> next = tail;
 			}
 			else{
+				//Node<T> *newNode = new Node<T>(el, tail -> next);
 				tail -> next = new Node<T>(el, tail -> next);
-				tail = tail -> next;
+				//tail = tail -> next;
 			}
 		}
+		
+		void display(void){
+			Node<T> *start = tail -> next;
+			Node<T> *temp = tail->next;
+			do{
+				cout<<temp -> data<<" << ";
+				temp = temp -> next;
+			}while( temp != start);
+			
+		}
 };
+
+int main(void){
+	CList <int>clst;
+	int temp, el;
+	while(true){
+		cout<<"\nEnter:\t1.Add to Head\n\t2.Add to Tail\n\t5.Display\n\t8.Exit:\t";
+		cin>>temp;
+		switch(temp){
+			case 1:{
+				cout<<"\nEnter Element:\t";
+				cin>>el;
+				clst.addToHead(el);
+				break;
+			}
+			case 5:{
+				clst.display();
+				break;
+			}
+			case 8:	return 0;
+		}
+	}
+}
